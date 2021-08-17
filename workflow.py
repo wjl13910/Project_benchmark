1e53ccbcbaa52f5baf83d1ff46a713aa8a882f2c
wjl13910 <836823863@qq.com>
2021-08-17 16:36:37 +0800

    update data files

--- workflow.py
+++ workflow.py
@@ -10,50 +10,50 @@ from models import model_function1, model_function2, model_function3, model_func
 from benchmark import load_data, fitting, calculate_rmse, general_plot, overall_workflow
 
 
-# syklake_data = load_data("./skylake_data.csv")
+syklake_data = load_data("./skylake_data.csv")
 
-# x = syklake_data["x"]
+x = syklake_data["x"]
 
 
-# # general plot
-# general_plot(syklake_data, "Skylake")
+# general plot
+general_plot(syklake_data, "Skylake")
 
 # CPU
 # three possible methods are: dogbox, lm, trf
-# cpu_parameters1 = fitting(
-#     x, syklake_data["CPU Time"], model_function1, "dogbox", "cpu_model1")
-# cpu_parameters2 = fitting(
-#     x, syklake_data["CPU Time"], model_function2, "dogbox", "cpu_model2")
+cpu_parameters1 = fitting(
+    x, syklake_data["CPU Time"], model_function1, "dogbox", "cpu_model1")
+cpu_parameters2 = fitting(
+    x, syklake_data["CPU Time"], model_function2, "dogbox", "cpu_model2")
 
-# cpu_rmse1 = calculate_rmse(
-#     x, syklake_data["CPU Time"], model_function1, cpu_parameters1)
-# cpu_rmse2 = calculate_rmse(
-#     x, syklake_data["CPU Time"], model_function2, cpu_parameters2)
+cpu_rmse1 = calculate_rmse(
+    x, syklake_data["CPU Time"], model_function1, cpu_parameters1)
+cpu_rmse2 = calculate_rmse(
+    x, syklake_data["CPU Time"], model_function2, cpu_parameters2)
 
-# # MB
-# mb_parameters1 = fitting(
-#     x, syklake_data["Memory Time"], model_function1, "lm", "memory_model1")
-# mb_parameters2 = fitting(
-#     x, syklake_data["Memory Time"], model_function2, "lm", "memory_model2")
+# MB
+mb_parameters1 = fitting(
+    x, syklake_data["Memory Time"], model_function1, "lm", "memory_model1")
+mb_parameters2 = fitting(
+    x, syklake_data["Memory Time"], model_function2, "lm", "memory_model2")
 
-# mb_rmse1 = calculate_rmse(
-#     x, syklake_data["Memory Time"], model_function1, mb_parameters1)
-# mb_rmse2 = calculate_rmse(
-#     x, syklake_data["Memory Time"], model_function2, mb_parameters2)
+mb_rmse1 = calculate_rmse(
+    x, syklake_data["Memory Time"], model_function1, mb_parameters1)
+mb_rmse2 = calculate_rmse(
+    x, syklake_data["Memory Time"], model_function2, mb_parameters2)
 
-# # IB
-# ib_parameters1 = fitting(
-#     x, syklake_data["MPI Time"], model_function3, "lm", "IB_model3")
-# ib_parameters2 = fitting(
-#     x, syklake_data["MPI Time"], model_function4, "lm", "IB_model4")
+# IB
+ib_parameters1 = fitting(
+    x, syklake_data["MPI Time"], model_function3, "lm", "IB_model3")
+ib_parameters2 = fitting(
+    x, syklake_data["MPI Time"], model_function4, "lm", "IB_model4")
 
-# ib_rmse1 = calculate_rmse(
-#     x, syklake_data["MPI Time"], model_function3, ib_parameters1)
-# ib_rmse2 = calculate_rmse(
-#     x, syklake_data["MPI Time"], model_function4, ib_parameters2)
+ib_rmse1 = calculate_rmse(
+    x, syklake_data["MPI Time"], model_function3, ib_parameters1)
+ib_rmse2 = calculate_rmse(
+    x, syklake_data["MPI Time"], model_function4, ib_parameters2)
 
 
-filepath = "./skylake_data.csv"
+# filepath = "./skylake_data.csv"
 
-overall_workflow(filepath, "Skylake", model_function2,
-                 model_function4, "lm")
+# overall_workflow(filepath, "Skylake", model_function2,
+#                  model_function4, "lm")
